be7de5c95acd55eb929e0d66ac7cdd2f22f14455
Jon Ludlam <jonathan.ludlam@citrix.com>
2016-09-28 17:02:52 +0100

    Remove some exception raises from Rpc_lwt
    Signed-off-by: Jon Ludlam <jonathan.ludlam@citrix.com>

--- lib/rpc_lwt.ml
+++ lib/rpc_lwt.ml
@@ -73,32 +73,32 @@ module GenServer = struct
     let open Rresult.R in
     let get_named_args call =
       match call.params with
-      | [Dict x] -> x
-      | _ -> raise (MarshalError "All arguments must be named currently")
+      | [Dict x] -> Lwt.return x
+      | _ -> Lwt.fail (MarshalError "All arguments must be named currently")
     in
 
     let get_arg args name =
       if not (List.mem_assoc name args)
-      then raise (MarshalError (Printf.sprintf "Argument missing: %s" name))
-      else List.assoc name args
+      then Lwt.fail (MarshalError (Printf.sprintf "Argument missing: %s" name))
+      else Lwt.return (List.assoc name args)
     in
 
     let rpcfn =
       let rec inner : type a. a fn -> a -> call -> response Lwt.t = fun f impl call ->
-        let args = get_named_args call in
+        Lwt.bind (get_named_args call) (fun args ->
         match f with
         | Function (t, f) -> begin
-            let arg_rpc = get_arg args t.Param.name in
+            Lwt.bind (get_arg args t.Param.name) (fun arg_rpc ->
             let z = Rpcmarshal.unmarshal t.Param.typedef.Rpc.Types.ty arg_rpc in
             match z with
             | Result.Ok arg -> inner f (impl arg) call
-            | Result.Error (`Msg m) -> raise (MarshalError m)
+            | Result.Error (`Msg m) -> Lwt.fail (MarshalError m))
           end
         | Returning (t,e) -> begin
             Lwt.bind impl.M.lwt (function
                   | Result.Ok x -> Lwt.return (success (Rpcmarshal.marshal t.Param.typedef.Rpc.Types.ty x))
                   | Result.Error y -> Lwt.return (failure (Rpcmarshal.marshal e.Rpc.Types.ty y)))
-          end
+          end)
       in inner ty impl
     in
 
