4706101eea4ab1ad776ecf26132d20bca0403698
Marcello Seri <marcello.seri@gmail.com>
2018-06-17 12:57:53 +0200

    Use new idl functor with lwt
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- src/lwt/rpc_lwt.mli
+++ src/lwt/rpc_lwt.mli
@@ -1,34 +1,27 @@
-type lwt_rpcfn = Rpc.call -> Rpc.response Lwt.t
-
-module M :
-  sig
-    type 'a lwt = { lwt : 'a Lwt.t; }
-    type ('a, 'b) t = ('a, 'b) Result.result lwt
-    val return : 'a -> ('a, 'b) t
-    val return_err : 'b -> ('a, 'b) t
-    val checked_bind : ('a, 'b) t -> ('a -> ('c, 'd) t) -> ('b -> ('c, 'd) t) -> ('c, 'd) t
-    val bind : ('a, 'b) t -> ('a -> ('c, 'b) t) -> ('c, 'b) t
-    val ( >>= ) : ('a, 'b) t -> ('a -> ('c, 'b) t) -> ('c, 'b) t
-    val lwt : 'a lwt -> 'a Lwt.t
-  end
+module LwtM : Idl.RPCMONAD
 
 (** Client generator similar to {!Idl.GenClient} that uses [Lwt]. *)
 module GenClient () :
   sig
+    open LwtM
     include Idl.RPC
       with type implementation = unit
-       and type 'a res = lwt_rpcfn -> 'a
-       and type ('a,'b) comp = ('a,'b) Result.result M.lwt
+       and type 'a res = rpcfn -> 'a
+       and type ('a,'b) comp = ('a,'b) t
   end
 
-type server_implementation
-val server : server_implementation -> lwt_rpcfn
-val combine : server_implementation list -> server_implementation
+module ServerImpl : sig
+  open LwtM
+  type server_implementation
+  val server : server_implementation -> rpcfn
+  val combine : server_implementation list -> server_implementation
+end
 
 (** Server generator similar to {!Idl.GenServer} that uses [Lwt]. *)
 module GenServer () : sig
+  open LwtM
   include Idl.RPC
-    with type implementation = server_implementation
+    with type implementation = (string, rpcfn option) Hashtbl.t
      and type 'a res = 'a -> unit
-     and type ('a,'b) comp = ('a,'b) Result.result M.lwt
+     and type ('a,'b) comp = ('a,'b) t
 end
