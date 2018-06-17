f43682076f180eea7fdac03f18ac7e0548e71ded
Marcello Seri <marcello.seri@gmail.com>
2018-06-17 12:58:02 +0200

    Use new idl functor with async
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- src/async/rpc_async.mli
+++ src/async/rpc_async.mli
@@ -1,41 +1,27 @@
-module Deferred = Async.Deferred
-
-type async_rpcfn = Rpc.call -> Rpc.response Deferred.t
-
-module M : sig
-  type 'a async = { async : 'a Deferred.t; }
-  type ('a, 'b) t = ('a, 'b) Result.result async
-  val return : 'a -> ('a, 'b) Result.result async
-  val return_err : 'a -> ('b, 'a) Result.result async
-  val checked_bind :
-    ('a, 'b) Result.result async ->
-    ('a -> 'c async) -> ('b -> 'c async) -> 'c async
-  val bind :
-    ('a, 'b) Result.result async ->
-    ('a -> ('c, 'b) Result.result async) -> ('c, 'b) Result.result async
-  val ( >>= ) :
-    ('a, 'b) Result.result async ->
-    ('a -> ('c, 'b) Result.result async) -> ('c, 'b) Result.result async
-  val deferred : 'a async -> 'a Deferred.t
-end
+module AsyncM : Idl.RPCMONAD
 
 (** Client generator similar to {!Idl.GenClient} that uses [Async]. *)
 module GenClient () :
   sig
+    open AsyncM
     include Idl.RPC
       with type implementation = unit
-       and type 'a res = async_rpcfn -> 'a
-       and type ('a,'b) comp = ('a,'b) Result.result M.async
+       and type 'a res = rpcfn -> 'a
+       and type ('a,'b) comp = ('a,'b) t
   end
 
-type server_implementation
-val server : server_implementation -> async_rpcfn
-val combine : server_implementation list -> server_implementation
+module ServerImpl : sig
+  open AsyncM
+  type server_implementation = (string, rpcfn option) Hashtbl.t
+  val server : server_implementation -> rpcfn
+  val combine : server_implementation list -> server_implementation
+end
 
 (** Server generator similar to {!Idl.GenServer} that uses [Async]. *)
 module GenServer () : sig
+  open AsyncM
   include Idl.RPC
-    with type implementation = server_implementation
+    with type implementation = (string, rpcfn option) Hashtbl.t
      and type 'a res = 'a -> unit
-     and type ('a,'b) comp = ('a,'b) Result.result M.async
+     and type ('a,'b) comp = ('a,'b) t
 end
