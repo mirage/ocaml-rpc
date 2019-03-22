4d542cb7b0c42a62196c6043ba556de54c1b7820
Pau Ruiz Safont <pau.safont@citrix.com>
2019-03-22 13:47:52 +0000

    maintenance: update js libraries
    Stop using deprecated interfaces for yojson and js_of_ocaml
    
    Signed-off-by: Pau Ruiz Safont <pau.safont@citrix.com>

--- src/js/rpc_client_js_helper.ml
+++ src/js/rpc_client_js_helper.ml
@@ -4,76 +4,76 @@
 
 let keys obj =
   let arr =
-    Js.Unsafe.meth_call
-      (Js.Unsafe.variable "Object")
+    Js_of_ocaml.Js.Unsafe.meth_call
+      (Js_of_ocaml.Js.Unsafe.variable "Object")
       "keys"
-      [|Js.Unsafe.inject obj|]
+      [|Js_of_ocaml.Js.Unsafe.inject obj|]
   in
-  List.map Js.to_string (Array.to_list (Js.to_array arr))
+  List.map Js_of_ocaml.Js.to_string (Array.to_list (Js_of_ocaml.Js.to_array arr))
 
 (* This is apparently the ECMAscript approved way of checking whether something is an array *)
 let is_array obj =
   let str =
-    Js.Unsafe.call (Js.Unsafe.variable "Object.prototype.toString") obj [||]
+    Js_of_ocaml.Js.Unsafe.call (Js_of_ocaml.Js.Unsafe.variable "Object.prototype.toString") obj [||]
   in
-  Js.to_string str = "[object Array]"
+  Js_of_ocaml.Js.to_string str = "[object Array]"
 
 (* Magic to find out whether something is one of the Js_of_ocaml Javascript string types *)
-let mlString_constr = Js.Unsafe.variable "MlString"
+let mlString_constr = Js_of_ocaml.Js.Unsafe.variable "MlString"
 
-let is_string obj = Js.instanceof obj mlString_constr
+let is_string obj = Js_of_ocaml.Js.instanceof obj mlString_constr
 
 (* Seems to work. I hope there's a better way of doing this! *)
-let nullobj = Json.unsafe_input (Js.string "null")
+let nullobj = Js_of_ocaml.Json.unsafe_input (Js_of_ocaml.Js.string "null")
 
-let is_null obj = Json.output obj = Js.string "null"
+let is_null obj = Js_of_ocaml.Json.output obj = Js_of_ocaml.Js.string "null"
 
 let rec rpc_of_json json =
-  let ty = Js.typeof json in
-  match Js.to_string ty with
+  let ty = Js_of_ocaml.Js.typeof json in
+  match Js_of_ocaml.Js.to_string ty with
   | "object" ->
       if is_array json then
-        let l = Array.to_list (Js.to_array json) in
+        let l = Array.to_list (Js_of_ocaml.Js.to_array json) in
         Rpc.Enum (List.map rpc_of_json l)
       else if is_string json then
-        Rpc.String (Js.to_string (Js.Unsafe.coerce json))
+        Rpc.String (Js_of_ocaml.Js.to_string (Js_of_ocaml.Js.Unsafe.coerce json))
       else if is_null json then Rpc.Null
       else
         let okeys = keys json in
         Rpc.Dict
           (List.map
-             (fun x -> (x, rpc_of_json (Js.Unsafe.get json (Js.string x))))
+             (fun x -> (x, rpc_of_json (Js_of_ocaml.Js.Unsafe.get json (Js_of_ocaml.Js.string x))))
              okeys)
-  | "boolean" -> Rpc.Bool (Js.to_bool (Obj.magic json))
+  | "boolean" -> Rpc.Bool (Js_of_ocaml.Js.to_bool (Obj.magic json))
   | "number" ->
       (* Convert all numbers to strings - the generic Rpc-light layer can deal with this *)
-      let str = Js.Unsafe.meth_call json "toString" [||] in
-      Rpc.String (Js.to_string str)
+      let str = Js_of_ocaml.Js.Unsafe.meth_call json "toString" [||] in
+      Rpc.String (Js_of_ocaml.Js.to_string str)
   | _ ->
       (* Datetime maybe? *)
-      Firebug.console ## log
-        (Js.string (Printf.sprintf "Ack! got %s" (Js.to_string ty))) ;
+      Js_of_ocaml.Firebug.console ## log
+        (Js_of_ocaml.Js.string (Printf.sprintf "Ack! got %s" (Js_of_ocaml.Js.to_string ty))) ;
       Rpc.Bool false
 
-let of_string s = rpc_of_json (Json.unsafe_input (Js.string s))
+let of_string s = rpc_of_json (Js_of_ocaml.Json.unsafe_input (Js_of_ocaml.Js.string s))
 
 (* Here be lots of magic. This is mostly untested *)
 let to_string rpc =
   let rec inner = function
     | Rpc.Dict kvs ->
-        let o = Json.unsafe_input (Js.string "{}") in
-        List.iter (fun (x, y) -> Js.Unsafe.set o (Js.string x) (inner y)) kvs ;
+        let o = Js_of_ocaml.Json.unsafe_input (Js_of_ocaml.Js.string "{}") in
+        List.iter (fun (x, y) -> Js_of_ocaml.Js.Unsafe.set o (Js_of_ocaml.Js.string x) (inner y)) kvs ;
         o
-    | Rpc.Int x -> Obj.magic (Js.string (Int64.to_string x))
+    | Rpc.Int x -> Obj.magic (Js_of_ocaml.Js.string (Int64.to_string x))
     | Rpc.Int32 x -> Obj.magic x
-    | Rpc.Float x -> Obj.magic (Js.string (string_of_float x))
-    | Rpc.String x -> Obj.magic (Js.string x)
-    | Rpc.Bool x -> Obj.magic (if x then Js._true else Js._false)
-    | Rpc.DateTime x -> Obj.magic (Js.string x)
-    | Rpc.Enum l -> Obj.magic (Js.array (Array.of_list (List.map inner l)))
-    | Rpc.Null -> Obj.magic Js.null
+    | Rpc.Float x -> Obj.magic (Js_of_ocaml.Js.string (string_of_float x))
+    | Rpc.String x -> Obj.magic (Js_of_ocaml.Js.string x)
+    | Rpc.Bool x -> Obj.magic (if x then Js_of_ocaml.Js._true else Js_of_ocaml.Js._false)
+    | Rpc.DateTime x -> Obj.magic (Js_of_ocaml.Js.string x)
+    | Rpc.Enum l -> Obj.magic (Js_of_ocaml.Js.array (Array.of_list (List.map inner l)))
+    | Rpc.Null -> Obj.magic Js_of_ocaml.Js.null
   in
-  Json.output (inner rpc)
+  Js_of_ocaml.Json.output (inner rpc)
 
 let new_id =
   let count = ref 0l in
@@ -88,7 +88,7 @@ let string_of_call call =
       ; ("params", Rpc.Enum call.Rpc.params)
       ; ("id", Rpc.Int32 (new_id ())) ]
   in
-  Js.to_string (to_string json)
+  Js_of_ocaml.Js.to_string (to_string json)
 
 exception Malformed_method_response of string
 
@@ -113,8 +113,8 @@ let response_of_string str =
           | Rpc.String s -> Int64.of_string s
           | _ -> failwith "inconsistent input"
         with _ ->
-          Firebug.console ## log
-            (Js.string
+          Js_of_ocaml.Firebug.console ## log
+            (Js_of_ocaml.Js.string
                (Printf.sprintf "Weirdness: %s" (Rpc.to_string (get "id" d)))) ;
           raise (Malformed_method_response "id")
       in
@@ -127,5 +127,5 @@ let response_of_string str =
                (Printf.sprintf "<result=%s><error=%s>" (Rpc.to_string x)
                   (Rpc.to_string y))) )
   | rpc ->
-      Firebug.console ## log (Js.string (Rpc.to_string rpc)) ;
+      Js_of_ocaml.Firebug.console ## log (Js_of_ocaml.Js.string (Rpc.to_string rpc)) ;
       failwith "Bah"
