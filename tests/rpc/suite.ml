cc98dbfb392e727ffd466f9a03ac611ec733d5b7
Marcello Seri <marcello.seri@gmail.com>
2020-03-03 21:31:08 +0100

    fixup! Run Lwt tests with Lwt

--- tests/rpc/suite.ml
+++ tests/rpc/suite.ml
@@ -3,6 +3,6 @@ let () =
     [ ("Client_async_new", Client_async_new.tests)
     ; ("Client_new", Client_new.tests)
     ; ("Test_pythongen", Test_pythongen.tests) ];
-  Lwt.run @@
+  Lwt_main.run @@
   Alcotest_lwt.run "rpc lwt test"
     [ ("Client_lwt_new", Client_lwt_new.tests) ]
