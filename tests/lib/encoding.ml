c5d8d8313f3bf6b6046cf3d9c3ddef8bc379d311
Marcello Seri <marcello.seri@gmail.com>
2020-04-09 20:45:30 +0200

    Add dumb tests
    we should really move to qcheck
    
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- tests/lib/encoding.ml
+++ tests/lib/encoding.ml
@@ -4,12 +4,25 @@ let run () =
   Printf.printf "r = %s\n%!" (Rpc.to_string r) ;
   let t' = Rpc.string_of_rpc r in
   Printf.printf "t = t : %b'\n%!" (t = t') ;
-  assert (t = t') ;
-  let test = Rpc.Dict [("foo", String "&")] in
-  let str = Xmlrpc.to_string test in
+  assert (t = t')
+
+let run_not_strict () =
+  let test = Rpc.Dict [("foo", Rpc.String "&"); ("bar", Rpc.Int (Int64.of_int 3))] in
+  let str = Xmlrpc.to_string ~strict:false test in
   assert (
     str
-    = "<value><struct><member><name>foo</name><value>&amp;</value></member></struct></value>"
+    = "<value><struct><member><name>foo</name><value>&amp;</value></member><member><name>bar</name><value>3</value></member></struct></value>"
    )
 
-let tests = [("test", `Quick, run)]
+let run_strict () =
+  let test = Rpc.Dict [("foo", Rpc.String "&"); ("bar", Rpc.Int (Int64.of_int 3))] in
+  let str = Xmlrpc.to_string ~strict:true test in
+  assert (
+    str
+    = "<value><struct><member><name>foo</name><value>&amp;</value></member><member><name>bar</name><value><i8>3</i8></value></member></struct></value>"
+   )
+
+let tests = [
+  ("test", `Quick, run);
+  ("test not strict", `Quick, run_not_strict);
+  ("test strict", `Quick, run_strict)]
