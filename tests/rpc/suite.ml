1d35101499f9270fc7b174bd4f66c4110af90c82
Marcello Seri <marcello.seri@gmail.com>
2020-04-16 20:05:27 +0200

    move lwt tests outside lwt
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- tests/rpc/suite.ml
+++ tests/rpc/suite.ml
@@ -1,7 +1,4 @@
 let () =
   Alcotest.run "rpc tests" [
       ("Client_new", Client_new.tests)
-    ; ("Test_pythongen", Test_pythongen.tests) ];
-  Lwt_main.run @@
-  Alcotest_lwt.run "rpc lwt test"
-    [ ("Client_lwt_new", Client_lwt_new.tests) ]
+    ; ("Test_pythongen", Test_pythongen.tests) ]
