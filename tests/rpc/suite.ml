ed29a22a4f0b1d4a1e8fafc530764454fd7ef84f
Marcello Seri <marcello.seri@gmail.com>
2020-10-04 13:37:14 +0200

    Skip python tests in absence of pylint
    Signed-off-by: Marcello Seri <marcello.seri@gmail.com>

--- tests/rpc/suite.ml
+++ tests/rpc/suite.ml
@@ -1,4 +1,11 @@
+let detect_pylint () =
+  if (abs @@ Sys.command "which pylint") + (abs @@ Sys.command "which pycodestyle") > 0 then
+    (print_endline "Warning: pylint or pycodestyle not found... Skipping python linting tests!";
+    false)
+  else true
+
 let () =
   Alcotest.run
     "rpc tests"
-    [ "Client_new", Client_new.tests; "Test_pythongen", Test_pythongen.tests ]
+    @@ [ "Client_new", Client_new.tests ]
+    @ if detect_pylint () then [ "Test_pythongen", Test_pythongen.tests ] else []
